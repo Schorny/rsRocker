923a4b95a1d37462771c0a3747ddd9fd8622bc75
Toni Schornboeck <toni.schornboeck@gmail.com>
2015-01-27 21:22:02 +0100

    initial commit

---
+++ config.js
@@ -0,0 +1,6 @@
+var rsR = rsR || {}
+
+rsR.config = {
+    rsBaseUrl: 'http://rockingsoccer.com/en/soccer/',
+
+}
---
+++ rsrocker.js
@@ -0,0 +1,50 @@
+(function(root, baseUrl, undefined) {
+
+
+    function initDependencies(baseUrl) {
+        var dfd = new $.Deferred();
+        $.when(
+            $.getScript(baseUrl+'config.js'),
+            $.getScript(baseUrl+'lib/loader.js'),
+            $.getScript(baseUrl+'lib/player.js')
+        ).done(function() {
+            dfd.resolve();
+        });
+
+        return dfd.promise();
+    }
+
+
+    $.when(initDependencies(baseUrl)).done(function() {
+
+        var parts=location.href.split("-");
+        if(parts.length!=2) {
+            console.log("parts is not 2");
+            return;
+        }
+
+        var playerid = parts[1];
+
+        $.when(rsR.loadPlayer(playerid)).done(function(player) {
+            var html =
+                "<span style='position:absolute;top:0;margin:auto;left:0;right:0;background-color:#ccc;width:180px'>" +
+                    "<center><table border='0'>" +
+                        "<caption>" + player.getName() + "</caption>" +
+                        "<tr><th align='left'>Scoring:</th><td align='right'>" + player.getRealScoring() + "</td></tr>" +
+                        "<tr><th align='left'>Passing:</th><td align='right'>" + player.getRealPassing() + "</td></tr>" +
+                        "<tr><th align='left'>Dueling:</th><td align='right'>" + player.getRealDueling() + "</td></tr>" +
+                        "<tr><th align='left'>Blocking:</th><td align='right'>" + player.getRealBlocking() + "</td></tr>" +
+                    "</table></center>" +
+                "</span>"
+            ;
+
+            $('body').append(html);
+        });
+    });
+
+
+
+})(document, "http://localhost/code/rsRocker/");
+
+//bookmarklet code:
+//javascript:(function(){var e=document.createElement('script');e.src='http://localhost/code/rsRocker/rsrocker.js';document.body.appendChild(e);})();
