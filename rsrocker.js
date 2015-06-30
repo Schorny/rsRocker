da192c9a90b9a5881b02795cee2ee28486dc3c40
Toni Schornboeck <toni.schornboeck@gmail.com>
2015-06-30 18:38:38 +0200

    created skeleton for v2

--- rsrocker.js
+++ rsrocker.js
@@ -3,19 +3,78 @@
 
     function initDependencies(baseUrl) {
         var dfd = new $.Deferred();
+        function loadCss(url) {
+            $('<link rel="stylesheet" type="text/css" href="'+url+'" />').appendTo($('head'));
+        }
+
+        $('<span id="rsr"></span>').appendTo($('body'));
+        $('<img src="'+baseUrl+'img/spinner.gif'+'" style="position:absolute;top:0;left:47%;" id="rsrspinner"/>').appendTo($('body'));
+        loadCss(baseUrl+'css/default.css');
+        loadCss(baseUrl+'css/ui.css');
+        loadCss('https://code.jquery.com/ui/1.11.3/themes/smoothness/jquery-ui.css');
+        //loadCss('https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css');
         $.when(
+            $.getScript('https://cdn.jsdelivr.net/i18next/1.7.7/i18next.min.js'),
+            $.getScript('https://code.jquery.com/ui/1.11.3/jquery-ui.min.js'),
+            $.getScript('https://cdnjs.cloudflare.com/ajax/libs/handlebars.js/2.0.0/handlebars.min.js'),
+            $.getScript('http://underscorejs.org/underscore-min.js'),
             $.getScript(baseUrl+'config.js'),
+            $.getScript(baseUrl+'lib/ui.js'),
+            $.getScript(baseUrl+'lib/util.js'),
             $.getScript(baseUrl+'lib/loader.js'),
-            $.getScript(baseUrl+'lib/player.js')
+            $.getScript(baseUrl+'lib/player.js'),
+            $.getScript(baseUrl+'lib/match.js'),
+            $.getScript(baseUrl+'lib/matchevents.js')
         ).done(function() {
-            dfd.resolve();
+            $.when($.getScript('https://cdnjs.cloudflare.com/ajax/libs/backbone.js/1.1.0/backbone-min.js')).done(function() {
+                rsR.config.baseUrl = baseUrl;
+                $.when(rsR.ui.init()).done(function() {
+
+                    Handlebars.registerHelper('t', function(text) {
+                        return i18n.t(text);
+                    });
+
+                    i18n.init({
+                        resGetPath: baseUrl+'locale/__lng__/__ns__.json',
+                        defaultNs: 'rsr',
+                        ns: 'rsr',
+                        lng: 'en',
+                        debug: false,
+                        fallbackLng: false,
+                        namespaces: ['rsr', 'skills', 'ui']
+                    }, function() {
+                        i18n.loadNamespaces(['skills', 'error', 'ui'], function() {
+                            $('#rsrspinner').remove();
+                            dfd.resolve();
+                        });
+                    });
+                });
+            });
         });
 
         return dfd.promise();
     }
 
 
+    function handlePlayerInfo(playerid) {
+        $.when(rsR.loadPlayer(playerid)).done(function(player) {
+
+            var frame = new rsR.ui.Frame({name: i18n.t("ui:playerinfo"), content: new rsR.ui.PlayerInfo({
+                player: player
+            })});
+        });
+    }
+
+    function test() {
+
+    }
+
     $.when(initDependencies(baseUrl)).done(function() {
+
+        var menu = new rsR.ui.MainMenu();
+
+        rsR.match.handleMatch();
+        return;
 
         var parts=location.href.split("-");
         if(parts.length!=2) {
@@ -24,27 +83,15 @@
         }
 
         var playerid = parts[1];
+        handlePlayerInfo(playerid);
 
-        $.when(rsR.loadPlayer(playerid)).done(function(player) {
-            var html =
-                "<span style='position:absolute;top:0;margin:auto;left:0;right:0;background-color:#ccc;width:180px'>" +
-                    "<center><table border='0'>" +
-                        "<caption>" + player.getName() + "</caption>" +
-                        "<tr><th align='left'>Scoring:</th><td align='right'>" + player.getRealScoring() + "</td></tr>" +
-                        "<tr><th align='left'>Passing:</th><td align='right'>" + player.getRealPassing() + "</td></tr>" +
-                        "<tr><th align='left'>Dueling:</th><td align='right'>" + player.getRealDueling() + "</td></tr>" +
-                        "<tr><th align='left'>Blocking:</th><td align='right'>" + player.getRealBlocking() + "</td></tr>" +
-                    "</table></center>" +
-                "</span>"
-            ;
-
-            $('body').append(html);
-        });
     });
 
 
 
-})(document, "https://rawgit.com/Schorny/rsRocker/master/");
+//})(document, "http://rawgit.com/Schorny/rsRocker/master/");
+})(document, "http://localhost/code/rsRocker/");
+
 
 //bookmarklet code:
 //javascript:(function(){var e=document.createElement('script');e.src='http://localhost/code/rsRocker/rsrocker.js';document.body.appendChild(e);})();
